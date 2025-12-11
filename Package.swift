8dd167ed778084490b1f46a110ded57c9030f312
Igor Ševtšenko <igorse2001@gmail.com>
2025-12-11 22:58:07 +0200

    Update Package.swift for version 1.4.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SplitApi",
-            url: "https://github.com/IgorShevtshenko/split-api/releases/download/1.2.0/splitapi.xcframework.zip",
-            checksum: "805ff347f97c7e50406070c67b7e4e3346acc3138fd3a15d34123e9deb576387"
+            url: "https://github.com/IgorShevtshenko/split-api/releases/download/1.4.0/splitapi.xcframework.zip",
+            checksum: "fcbe03658d2e52d45f7fded1be2f8ea6233dd2e6baf8704bb58de07768c20107"
         ),
     ]
 )
