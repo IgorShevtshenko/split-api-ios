4fc4b76762c9ee0e8a37ddc9c2ae455c2a216aa5
Igor Ševtšenko <igorse2001@gmail.com>
2025-12-11 23:31:58 +0200

    Update Package.swift for version 1.5.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
     name: "SplitApi",
-    platforms: [.iOS(.v17)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "SplitApi",
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SplitApi",
-            url: "https://github.com/IgorShevtshenko/split-api/releases/download/1.4.0/splitapi.xcframework.zip",
-            checksum: "fcbe03658d2e52d45f7fded1be2f8ea6233dd2e6baf8704bb58de07768c20107"
+            url: "https://github.com/IgorShevtshenko/split-api/releases/download/1.5.0/splitapi.xcframework.zip",
+            checksum: "10387be3f2f057eec9981a2f577c30c3a86a73b9d167e1018993c6345f460e1e"
         ),
     ]
 )
