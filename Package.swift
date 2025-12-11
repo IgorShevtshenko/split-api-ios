17ad731ba4220fe52516a5776e7a05a18064fd71
Igor Ševtšenko <igorse2001@gmail.com>
2025-12-11 23:49:46 +0200

    Use zip al local file

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.10
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
     name: "SplitApi",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v17)],
     products: [
         .library(
             name: "SplitApi",
@@ -13,8 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SplitApi",
-            url: "https://github.com/IgorShevtshenko/split-api/releases/download/1.5.0/splitapi.xcframework.zip",
-            checksum: "10387be3f2f057eec9981a2f577c30c3a86a73b9d167e1018993c6345f460e1e"
+             path: "./splitapi.xcframework.zip"
         ),
     ]
 )
